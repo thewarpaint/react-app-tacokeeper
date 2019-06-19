a11a7c669ec18f2d5eb2142e7a9273c8148ed249
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2019-06-19 18:20:18 -0500

    Extract tweet helpers to their own file

--- src/components/TweetText.jsx
+++ src/components/TweetText.jsx
@@ -1,39 +1,11 @@
 import React from 'react';
 
-function formatList(terms) {
-  const conjunctionText = ' y ';
-
-  // Works for arrays with length 0 (empty string), 1 ('first'), or 2 ('first and second')
-  if (terms.length < 3) {
-    return terms.join(conjunctionText);
-  }
-
-  return terms.slice(0, terms.length - 1).join(', ') + conjunctionText + terms.slice(-1);
-}
-
-function getPluralisedVariety(variety) {
-  return variety.value +
-    (variety.value === 1
-      ? ' taco ' + variety.forms.singular
-      : ' tacos ' + variety.forms.plural);
-}
-
-function getText(varieties) {
-  const initialText = 'Hoy comí';
-
-  const activeVarieties = varieties.filter(variety => variety.value > 0);
-
-  if (!activeVarieties.length) {
-    return initialText + '...';
-  }
-
-  return `${initialText} ${formatList(activeVarieties.map(getPluralisedVariety))}`;
-};
+import {getTweetText} from '../tweetHelpers';
 
 const TweetText = (props) => {
   return (
     <div className="tweet-text">
-      {getText(props.varieties)}
+      {getTweetText(props.varieties)}
     </div>
   );
 };
---
+++ src/tweetHelpers.js
@@ -0,0 +1,29 @@
+function formatList(terms) {
+  const conjunctionText = ' y ';
+
+  // Works for arrays with length 0 (empty string), 1 ('first'), or 2 ('first and second')
+  if (terms.length < 3) {
+    return terms.join(conjunctionText);
+  }
+
+  return terms.slice(0, terms.length - 1).join(', ') + conjunctionText + terms.slice(-1);
+}
+
+function getPluralisedVariety(variety) {
+  return variety.value +
+    (variety.value === 1
+      ? ' taco ' + variety.forms.singular
+      : ' tacos ' + variety.forms.plural);
+}
+
+export function getTweetText(varieties) {
+  const initialText = 'Hoy comí';
+
+  const activeVarieties = varieties.filter(variety => variety.value > 0);
+
+  if (!activeVarieties.length) {
+    return initialText + '...';
+  }
+
+  return `${initialText} ${formatList(activeVarieties.map(getPluralisedVariety))}`;
+}
